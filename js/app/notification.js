bb2a482b67a40ac50db72ff83b6f792778ac682c
Maxim Arhipov <develop5@masproject.pro>
2019-03-10 19:11:58 +0800

    потом по клику сделаем поиск ...

--- js/app/notification.js
+++ js/app/notification.js
@@ -1,16 +1,18 @@
 import {getNameStatus} from '../helper/helperStatus';
+import {search} from "../action/table";
 
 export const sendNotification = (type, data) => {
     Notification.requestPermission().then(permission => {
         if (permission === 'denied') return false;
         if (type === 'CHANGE_STATUS') {
-            new Notification('Изменен статус', {
+            const notification = new Notification('Изменен статус', {
                 body: `Изменен статус у заказа №${data.NUMBER}, на "${getNameStatus(data.STATUS, false)}"`,
-                onclick: e => {
-                    debugger;
-
-                },
+                data: data.NUMBER,
             });
+            /*notification.onclick = e => {
+                debugger;
+                search(window.store.dispatch, 1, '', notification.data);
+            }*/
         }
     });
 };
